5c01b81b77ed0ed3fc711dc9ce34ea8c2abff89c
asadlari <asad.lari123@gmail.com>
2025-08-03 14:28:08 +0530

    Initial commit

---
+++ src/main/java/com/railse/hiring/workforcemgmt/common/model/response/ResponseStatus.java
@@ -0,0 +1,21 @@
+package com.railse.hiring.workforcemgmt.common.model.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ResponseStatus {
+    private Integer code;
+    private String message;
+
+//    public ResponseStatus(int code, String message) {
+//        this.code=code;
+//        this.message=message;
+//    }
+}
